4cd6845bcbae1524e8fb381071ffb4506b412751
Ben Myerson <benmyerson@gmail.com>
2016-10-15 21:39:23 -0700

    Account link (#9)
    * All kinds of account linkage nonsense
    
    * remove unneeded registration page
    
    * link and unlink providers

--- app/src/components/account-account/account-account.js
+++ app/src/components/account-account/account-account.js
@@ -4,17 +4,83 @@ import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import templateUrl from './account-account.html';
 
+
+function isProviderEnabled(user, pro) {
+    for (var provider in user.providerData) {
+        if (user.providerData[provider].providerId === pro) {
+            return true;
+        }
+    }
+    return false;
+}
+
 class AccountAccount {
-    constructor($state, Auth) {
+    constructor($state, $scope, Auth, firebase) {
         this.Auth = Auth;
         this.$state = $state;
+        this.$scope = $scope;
+
+        let goog = new firebase.auth.GoogleAuthProvider(),
+            fb = new firebase.auth.FacebookAuthProvider(),
+            tw = new firebase.auth.TwitterAuthProvider(),
+            gh = new firebase.auth.GithubAuthProvider();
+        this.providers = {
+            'google': {
+                provider: goog,
+                isLinked: isProviderEnabled(this.user, goog.providerId)
+            },
+            'facebook':  {
+                provider: fb,
+                isLinked: isProviderEnabled(this.user, fb.providerId)
+            },
+            'twitter':  {
+                provider: tw,
+                isLinked: isProviderEnabled(this.user, tw.providerId)
+            },
+            'github':  {
+                provider: gh,
+                isLinked: isProviderEnabled(this.user, gh.providerId)
+            }
+        };
+    }
+
+    onlyOneProvider() {
+        return this.user.providerData.length === 1;
+    }
+
+    setProvider(pro) {
+        let isCurrentlyEnabled = isProviderEnabled(this.user, pro.provider.providerId);
+        if (isCurrentlyEnabled != pro.isLinked) {
+            if (pro.isLinked) {
+                this.user.linkWithPopup(pro.provider)
+                    .then(() => {
+                        this.$scope.$apply();
+                    })
+                    .catch((error) => {
+                        this.$scope.$apply(() => {
+                            pro.isLinked = false;
+                            console.error(error);
+                        });
+                    });
+            } else {
+                // unlink stuffs
+                this.user.unlink(pro.provider.providerId)
+                    .then(() => {
+                        this.$scope.$apply();
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                    })
+            }
+        }
     }
 }
+
 const name = 'accountAccount';
 export default angular.module(name, [uiRouter])
     .component(name, {
         templateUrl,
-        controller: ['$state', 'Auth', AccountAccount],
+        controller: ['$state', '$scope', 'Auth', 'firebase', AccountAccount],
         bindings: {
             user: '='
         }
@@ -23,12 +89,7 @@ export default angular.module(name, [uiRouter])
         $stateProvider
             .state('account.account', {
                 url:'/account',
-                template: '<account-account user="$resolve.currentAuth"></account-account>',
-                resolve: {
-                    "currentAuth": ["Auth", function(Auth) {
-                        return Auth.$requireSignIn();
-                    }]
-                }
+                template: '<account-account user="$resolve.currentAuth"></account-account>'
             });
     }])
     .name;
